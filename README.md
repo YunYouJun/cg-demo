35d81c31a8b98116048fbf6064e9c1652afaaa43
YunYouJun <me@yunyoujun.cn>
2023-08-01 01:51:28 +0800

    feat: add postprocessing msaa

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Demo for Computer Graphics.
 
 > 图解计算机图形学。
+
+## three.js for demo
+
+- postprocessing
+  - [How to use post-processing?](https://threejs.org/docs/?q=post#manual/en/introduction/How-to-use-post-processing)
+  - [pmndrs/postprocessing](https://github.com/pmndrs/postprocessing)
--- components/aa/MSAA.vue
+++ components/aa/MSAA.vue
@@ -1,5 +1,26 @@
+<script lang="ts" setup>
+import { initMSAA } from '~/utils/aa/MSAA'
+
+const containerRef = ref<HTMLElement>()
+
+onMounted(() => {
+  if (!containerRef.value)
+    return
+  initMSAA(containerRef.value)
+})
+</script>
+
 <template>
   <div>
     MSAA
+    <div id="container" ref="containerRef" />
   </div>
 </template>
+
+<style>
+#container {
+  position: absolute;
+  inset: 0;
+  width: 100%;
+}
+</style>
